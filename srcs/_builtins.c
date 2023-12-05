dfe13008864cd7dfa19271de4569526e6a82f717
rabatm <martin.Rabat@gmail.Com>
2023-12-05 16:06:35 +0100

    Add ft echo

--- srcs/_builtins.c
+++ srcs/_builtins.c
@@ -1,1 +1,24 @@
 #include "../includes/minishell.h"
+
+int ft_echo(int argc, char** argv, char **env)
+{
+	int	ret;
+	int	i;
+
+	ret = 0;
+	i = 0;
+
+	printf("");
+	while (i < argc)
+	{
+		if (argv[i] != "-n")
+			printf("%s", argv[i]);
+		i++;
+	}
+	if (argv[0] == '-n')
+		printf("\n");
+	// TODO
+
+	return (ret);
+	// return (1 ou 0)
+}
